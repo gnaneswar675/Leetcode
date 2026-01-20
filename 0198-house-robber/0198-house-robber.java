8cd087a1f6aecc0af094885ea0c2049b0fc9bef5
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-01-20 14:53:48 +0530

    Time: 0 ms (100%), Space: 42.8 MB (43.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
         if (nums.length==0) return 0;
-        int[] memo =new int[nums.length+1];
-        memo[0]=0;
-        memo[1]=nums[0];
+        int[] dp =new int[nums.length+1];
+        dp[0]=0;
+        dp[1]=nums[0];
         for (int i=1; i <nums.length;i++) 
         {
             int val=nums[i];
-            memo[i+1]=Math.max(memo[i],memo[i-1] + val);
+            dp[i+1]=Math.max(dp[i],dp[i-1] + val);
         }
-        return memo[nums.length];
+        return dp[nums.length];
     }
 }
