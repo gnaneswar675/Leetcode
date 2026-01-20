d2d7ba259397f18efc46a221d3fdad6ba257f006
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-01-20 13:28:14 +0530

    Time: 0 ms (100%), Space: 42.1 MB (61.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
         if(n<=3)return n;
-        int pr1=3,pr2=2,c=0;
+    int pr1=3,pr2=2,c=0;
         for(int i=3;i<n;i++){
             c=pr1+pr2;
             pr2=pr1;
             pr1=c;
         }return c;
+        
     }
 }
