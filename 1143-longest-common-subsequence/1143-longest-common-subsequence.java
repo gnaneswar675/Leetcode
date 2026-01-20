f062410f6df1ea23e9464ab2968021fece3a3c8d
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-01-20 15:43:00 +0530

    Time: 19 ms (88.58%), Space: 53.9 MB (73.62%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -14,12 +14,5 @@ class Solution {
             }
         }return dp[m][n];
     }
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("-1");
-            } catch (Exception e) {
-            }
-        }));
-    }
+    
 }
