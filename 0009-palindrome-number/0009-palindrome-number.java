ea7f6d5d55fbc625553abc950b9a1cbe66ce33d8
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-04-17 23:09:41 +0530

    Time: 5 ms (79.71%), Space: 46.1 MB (21.39%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int t=x;
-        int rem,sum=0;
-        
+        int rem,sum=0;        
         if (x<0)
         return false;
         while (x!=0)
