5fddc6029caf7648a154b98e1f5bd49dae3e4c38
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-06-11 22:06:04 +0530

    Time: 0 ms (100%), Space: 45.4 MB (77.98%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,4 +1,5 @@
-class Solution {
+//using extra space
+/*class Solution {
     public static void traverse(int nums[],boolean[] flag,List<Integer> list,List<List<Integer>> res){
         if(list.size()==nums.length){
             res.add(new ArrayList<>(list));
@@ -20,6 +21,35 @@ class Solution {
         List<List<Integer>>res=new ArrayList<>();
         boolean[] flag=new boolean[nums.length];
         traverse(nums,flag,list,res);
+        return res;
+    }
+}
+*/
+//without extra space
+class Solution {
+    public static void traverse(int nums[],int cur,List<List<Integer>> res){
+        if(cur==nums.length){
+           List<Integer>list=new ArrayList<>();
+           for(int i=0;i<nums.length;i++){
+            list.add(nums[i]);
+           }res.add(list);
+           return;
+        }
+        for(int i=cur;i<nums.length;i++){
+            swap(i,cur,nums);
+            traverse(nums,cur+1,res);
+            swap(i,cur,nums);
+        }
+    }
+    public static void swap(int a,int b,int nums[]){
+        int t=nums[a];
+        nums[a]=nums[b];
+        nums[b]=t;
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer>list=new ArrayList<>();
+        List<List<Integer>>res=new ArrayList<>();        
+        traverse(nums,0,res);
         return res;
     }
 }
