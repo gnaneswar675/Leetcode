288b89a00dc906f9c29664e43b2473ebccb36a33
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-07-12 20:38:16 +0530

    Time: 0 ms (100%), Space: 47 MB (54.04%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -16,12 +16,13 @@
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if(root==null)return null;
-        if(root.val==val){
-            return root;
-        }else if(val<root.val){
+        if(root.val==val)return root;
+        if(val<root.val){
             return searchBST(root.left,val);
-        }else{
-            return searchBST(root.right,val);
         }
+        
+        return searchBST(root.right,val);
+        
+        
     }
 }
