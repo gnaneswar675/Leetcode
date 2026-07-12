8dcce74db3fb62ccbe0a9353c50944d3f05a33bf
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-07-12 17:44:12 +0530

    Time: 0 ms (100%), Space: 45.2 MB (14.57%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,12 +14,13 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetsum) {
-        if(root==null) return false;
-        if(root.left==null && root.right==null){
-            return targetsum-root.val==0;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null){
+            return false;
         }
-        targetsum=targetsum-root.val;
-        return hasPathSum(root.left,targetsum)||hasPathSum(root.right,targetsum);
+        if(root.right==null && root.left==null)
+            return targetSum-root.val==0;
+        targetSum-=root.val;
+        return hasPathSum(root.left,targetSum)||hasPathSum(root.right,targetSum);
     }
 }
