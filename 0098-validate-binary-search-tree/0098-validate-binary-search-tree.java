1248b7ea46c33a9a673ea3d492f26d40962853d4
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-07-12 21:04:08 +0530

    Time: 0 ms (100%), Space: 45.1 MB (65.65%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,12 +15,12 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
+        if(root==null)return true;
         return traverse(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
-    public boolean traverse(TreeNode root,long minv,long maxv)
-    {
+    public boolean traverse(TreeNode root,long minn,long maxx){
         if(root==null)return true;
-        if(root.val<=minv || root.val>=maxv)return false;
-        return (traverse(root.left,minv,root.val)&&traverse(root.right,root.val,maxv));
+        if(root.val<=minn || root.val>=maxx)return false;
+        return traverse(root.left,minn,root.val) && traverse(root.right,root.val,maxx);
     }
 }
