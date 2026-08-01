09d4b98dd4e3aaf3db2522fe467c3dbac7a13339
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-08-01 12:39:42 +0530

    Time: 4 ms (91.35%), Space: 46.9 MB (89.1%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,29 +1,35 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
-
-        int[] indegree = new int[numCourses];
-        for (int[] pre : prerequisites) {
-            adj.get(pre[1]).add(pre[0]);
-            indegree[pre[0]]++;
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++)
-            if (indegree[i] == 0) q.add(i);
-
-        int count = 0;
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            count++;
-
-            for (int next : adj.get(node)) {
-                indegree[next]--;
-                if (indegree[next] == 0) q.add(next);
+        for(int edge[]:prerequisites){
+            adj.get(edge[0]).add(edge[1]);
+        }
+        int[] vis=new int[numCourses];
+        int[] dfsvis=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]==0){
+                if(traverse_dfs(i,adj,vis,dfsvis))
+                    return false;
             }
         }
-
-        return count == numCourses;
+        return true;
+    }
+    public boolean traverse_dfs(int cur,List<List<Integer>> adj,int vis[],int dfsvis[]){
+        vis[cur]=1;
+        dfsvis[cur]=1;
+        for(int num:adj.get(cur)){
+            if(vis[num]==0){
+                if(traverse_dfs(num,adj,vis,dfsvis))
+                return true;
+            }            
+            else if(dfsvis[num]==1)
+                return true;          
+            
+        }
+        dfsvis[cur]=0;
+        return false;
     }
 }
