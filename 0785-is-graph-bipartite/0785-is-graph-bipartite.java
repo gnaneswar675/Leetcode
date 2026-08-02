f57245d9f75de870d4e2e7ad8cb99f465de3aa47
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-08-02 11:05:10 +0530

    Time: 0 ms (100%), Space: 47.3 MB (19.86%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -5,6 +5,37 @@ class Solution {
             color[i]=-1;
         for(int i=0;i<graph.length;i++){
             if(color[i]==-1){
+                if(dfs(i,0,color,graph)==false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean dfs(int cur,int col,int color[],int[][] graph){
+        color[cur]=col;
+        for(int nei:graph[cur]){
+            if(color[nei]==-1){
+                if(dfs(nei,1-col,color,graph)==false){
+                    return false;
+                }
+            }
+            else if(color[nei]==color[cur])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
+/*class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int color[]=new int[graph.length];
+        for(int i=0;i<graph.length;i++)
+            color[i]=-1;
+        for(int i=0;i<graph.length;i++){
+            if(color[i]==-1){
                 if(bfs(i,color,graph)==false){
                     return false;
                 }
@@ -30,4 +61,4 @@ class Solution {
         }
         return true;
     }
-}
+}*/
