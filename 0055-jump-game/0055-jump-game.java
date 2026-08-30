4634bb63b8b7d8a25fb7884720c9774ac8bacf99
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-08-30 18:20:20 +0530

    Time: 2 ms (89.04%), Space: 47.9 MB (42.92%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,10 @@
 class Solution {
-    public boolean canJump(int[] A) {
-  int r = 0;
-  for (int i = 0; i < A.length; i++)
-    if (i <= r) r = Math.max(r, i + A[i]);
-  return r >= A.length - 1;
-}
-
+    public boolean canJump(int[] nums) {
+        int far=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>far)return false;
+            far=Math.max(far,i+nums[i]);
+        }
+        return true;
+    }
 }
