9e383822d074fbe60a375a520f2bd38da93994c1
Gnaneswar Thavva <thavvagnaneswar675@gmail.com>
2026-09-05 16:51:40 +0530

    Time: 15 ms (60.07%), Space: 51.5 MB (55.78%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,14 +1,13 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);Arrays.sort(s);
-        int res=0;
-        int i=0,j=0;
-        while(i<g.length&&j<s.length){
-            if(s[j]>=g[i]){
-                res++;i++;j++;
-            }else{
-                j++;
+        int i=0,j=0,res=0;
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i] ){
+                res++;
+                i++;
             }
+            j++;
         }
         return res;
     }
